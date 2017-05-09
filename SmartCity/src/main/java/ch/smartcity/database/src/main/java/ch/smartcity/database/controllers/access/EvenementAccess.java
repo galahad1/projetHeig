cca14ccd5bbd7621edd15eb145bc118aac4ab880
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-05-09 21:44:00 +0200

    Ajout de la fonction save plsu simple pour Tano

--- SmartCity/src/main/java/ch/smartcity/database/src/main/java/ch/smartcity/database/controllers/access/EvenementAccess.java
+++ SmartCity/src/main/java/ch/smartcity/database/src/main/java/ch/smartcity/database/controllers/access/EvenementAccess.java
@@ -257,6 +257,105 @@ public class EvenementAccess {
         return evenementList;
     }
 
+    public static void save(String nomRubriqueEnfant,
+                            Integer idUtilisateur,
+                            String nomEvenement,
+                            String nomRue,
+                            String numeroDeRue,
+                            String numeroNpa,
+                            Double latitude,
+                            Double longitude,
+                            Calendar debut,
+                            Calendar fin,
+                            String details,
+                            String nomPriorite,
+                            Integer niveauPriorite,
+                            String nomStatut) {
+        List<RubriqueEnfant> rubriqueEnfantList = RubriqueEnfantAccess
+                .get("", nomRubriqueEnfant);
+
+        boolean success = rubriqueEnfantList != null && rubriqueEnfantList.size() <= 1;
+        if (success) {
+            RubriqueEnfant rubriqueEnfant = rubriqueEnfantList.get(0);
+
+            Utilisateur administrator = DatabaseAccess.get(Utilisateur.class, idUtilisateur);
+            success = administrator != null;
+            if (success) {
+
+                List<Rue> rueList = RueAccess.get(nomRue);
+                success = rueList != null && rueList.size() <= 1;
+                if (success) {
+                    Rue rue = new Rue(nomRue);
+
+                    if (rueList.size() == 1) {
+                        rue = rueList.get(0);
+                    }
+
+                    List<Npa> npaList = NpaAccess.get(numeroNpa);
+                    success = npaList != null && npaList.size() <= 1;
+                    if (success) {
+                        Npa npa = new Npa(numeroNpa);
+
+                        if (npaList.size() == 1) {
+                            npa = npaList.get(0);
+                        }
+
+                        List<Adresse> adresseList = AdresseAccess.get(rue, numeroDeRue, npa);
+                        success = adresseList != null && adresseList.size() <= 1;
+                        if (success) {
+                            Adresse adresse = new Adresse(rue, numeroDeRue, npa);
+
+                            if (adresseList.size() == 1) {
+                                adresse = adresseList.get(0);
+                            }
+
+                            List<Priorite> prioriteList = PrioriteAccess.get(nomPriorite, niveauPriorite);
+                            success = prioriteList != null && prioriteList.size() <= 1;
+                            if (success) {
+                                Priorite priorite = new Priorite(nomPriorite, niveauPriorite);
+
+                                if (prioriteList.size() == 1) {
+                                    priorite = prioriteList.get(0);
+                                }
+
+                                List<Statut> statutList = StatutAccess.get(nomStatut);
+                                success = statutList != null && statutList.size() <= 1;
+                                if (success) {
+                                    Statut statut = new Statut(nomStatut);
+
+                                    if (statutList.size() == 1) {
+                                        statut = statutList.get(0);
+                                    }
+
+                                    save(rubriqueEnfant,
+                                            administrator,
+                                            nomEvenement,
+                                            adresse,
+                                            latitude,
+                                            longitude,
+                                            debut,
+                                            fin,
+                                            details,
+                                            priorite,
+                                            statut);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (success) {
+            getLogger().info(getConfigurationManager()
+                    .getString("databaseAccess.successInSubQuery"));
+
+        } else {
+            getLogger().info(getConfigurationManager()
+                    .getString("databaseAccess.errorInSubQuery"));
+        }
+    }
+
     public static void save(RubriqueEnfant rubriqueEnfant,
                             Utilisateur utilisateur,
                             String nomEvenement,
