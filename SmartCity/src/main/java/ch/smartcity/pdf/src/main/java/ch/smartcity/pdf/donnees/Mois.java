e64710fb350be7a8161186755e55687d753c4474
Lou <luana.martelli@heig-vd.ch>
2017-05-16 16:24:52 +0200

    Add PDF stuff, access to dataBase, not connected to graphic interface

--- SmartCity/src/main/java/ch/smartcity/pdf/src/main/java/ch/smartcity/pdf/donnees/Mois.java
+++ SmartCity/src/main/java/ch/smartcity/pdf/src/main/java/ch/smartcity/pdf/donnees/Mois.java
@@ -1,21 +1,9 @@
 package ch.smartcity.pdf.donnees;
 
+/**
+ * Created by Blue Spring on 15.05.2017.
+ */
 public enum Mois {
-    JANVIER,
-    FEVRIER,
-    MARS,
-    AVRIL,
-    MAI,
-    JUIN,
-    JUILLET,
-    AOUT,
-    SEPTEMBRE,
-    OCTOBRE,
-    NOVEMBRE,
-    DECEMBRE;
-
-    public String toString() {
-        return name().toLowerCase();
-    }
+    JANVIER, FEVRIER, MARS, AVRIL, MAI, JUIN, JUILLET, AOUT, SEPTEMBRE, OCTOBRE, NOVEMBRE, DECEMBRE
 }
 
