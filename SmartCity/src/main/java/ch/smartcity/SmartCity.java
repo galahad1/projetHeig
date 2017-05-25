f748765228b36a3342ebf3bf7b6f565876a1d52e
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-05-25 13:15:43 +0200

    Mise au propre du main de Smartcity v2

--- SmartCity/src/main/java/ch/smartcity/SmartCity.java
+++ SmartCity/src/main/java/ch/smartcity/SmartCity.java
@@ -5,7 +5,6 @@ import ch.smartcity.graphique.FenetrePrincipale;
 public class SmartCity {
 
     public static void main(String[] args) throws Exception {
-        FenetrePrincipale fenetre = new FenetrePrincipale();
-        fenetre.fenetre.setVisible(true);
+        new FenetrePrincipale().fenetre.setVisible(true);
     }
 }
